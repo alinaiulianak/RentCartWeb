3a9489d1ec50cccf0a61ef6bf4d5a627c54788b7
Alina <tical.alina@yahoo.com>
2019-10-27 13:25:38 +0200

    Base Project Setup

---
+++ RentCartWeb/RentCartWeb.WebUI/Controllers/CustomerManagerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.RentCartWeb.Core;
+using System.RentCartWeb.DataAccess.InMemory;
+
+
+namespace RentCartWeb.WebUI.Controllers
+{
+    public class CustomerManagerController : Controller
+    {
+        // GET: CustomerManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
